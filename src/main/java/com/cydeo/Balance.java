2925e346ffc69d0b28fe275f3db312d2b86130e4
omrzcn <omrzcnn@gmail.com>
2024-04-01 00:58:13 +0300

    loosely couple

---
+++ src/main/java/com/cydeo/Balance.java
@@ -0,0 +1,18 @@
+package com.cydeo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public abstract class Balance {
+    private UUID userId;
+    private BigDecimal amount;
+    public abstract void addBalance(BigDecimal amount);
+
+}
